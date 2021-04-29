976c09c01af5ea21da3df1149c907583445b4db3
ml4 <ml4@psy.industries>
2021-04-29 14:59:23 +0100

    Fix output sub submit back to id from name

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@
 // }
 
 output "avn-avn-main-subnet-id" {
-  value       = azurerm_virtual_network.main.subnet[0].name
+  value       = azurerm_virtual_network.main.subnet[0].id
   description = "Output the VNet subnet ID"
 }
