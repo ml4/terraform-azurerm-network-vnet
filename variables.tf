cd308d0aa4a10d2a10cbd872a4d63b7c4435cf19
ml4 <ml4@psy.industries>
2021-05-01 23:24:58 +0100

    Fix address space vars

--- variables.tf
+++ variables.tf
@@ -14,42 +14,43 @@ variable "location" {
   default     = "West Europe"
 }
 
-variable "sg_name" {
-  type        = string
-  description = "Name of the security group"
-}
+// variable "ddos_plan_name" {
+//   type        = string
+//   description = "Name of the vnet ddos plan"
+// }
 
-variable "ddos_plan_name" {
-  type        = string
-  description = "Name of the vnet ddos plan"
-}
-
-variable "vnet_name" {
-  type        = string
-  description = "Name of the vnet ddos plan"
-}
+// variable "vnet_name" {
+//   type        = string
+//   description = "Name of the vnet ddos plan"
+// }
 
 variable "public_ip_allowlist" {
   type        = list(string)
   default     = ["0.0.0.0/0"]       // demo only
 }
 
-variable "subnet_address_spaces" {
+variable "vnet_address_spaces" {
   type        = list(string)
   description = "CIDR ranges for vnet"
   default     = ["10.0.0.0/16"]
 }
 
-variable "private_subnet_name" {
-  type        = string
-  description = "Name of a single private vnet subnet"
+variable "subnet_address_spaces" {
+  type        = list(string)
+  description = "CIDR ranges for vnet"
+  default     = ["10.0.1.0/16"]
 }
 
-variable "private_subnet_cidr" {
-  type        = string
-  description = "CIDR of single private vnet subnet"
-  default     = "10.0.1.0/24"
-}
+// variable "private_subnet_name" {
+//   type        = string
+//   description = "Name of a single private vnet subnet"
+// }
+
+// variable "private_subnet_cidr" {
+//   type        = string
+//   description = "CIDR of single private vnet subnet"
+//   default     = "10.0.1.0/24"
+// }
 
 variable "common_tags" {
   type        = map(string)
