8f28791b085b77eef52563a59fb2fda4e84673c0
ml4 <ml4@psy.industries>
2021-05-02 22:25:35 +0100

    Add subnet_address_spaces

--- variables.tf
+++ variables.tf
@@ -25,3 +25,9 @@ variable "vnet_address_space" {
   description = "(Optional) list of vnet address ranges"
   default = ["10.0.0.0/16"]
 }
+
+variable "subnet_address_spaces" {
+  type = list(string)
+  description = "(Optional) list of subnet address ranges"
+  default = ["10.0.1.0/16"]
+}
