85711bccea906c959644792d7301c27a2a8e06b3
ml4 <ml4@psy.industries>
2021-06-09 16:06:15 +0100

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,32 @@
+//// outputs.tf child module terraform configuration
+//// Nomenclature: <d>-<e>-<f>-<g>[-<h>]
+//// where
+//// d = linked resource abbreviation (some resources in a mod are only there to support a main resource)
+//// e = abbreviated resource
+//// f = resource name
+//// g = resource parameter to output
+//// h = resource subparameter if applicable
+//
+//// and a, b and c get added in the root module
+//
+//// examples
+//
+// output "av-av-main-id" {
+//   value       = aws_vpc.main.id
+//   description = "AWS VPC: VPC ID"
+// }
+//
+// output "av-ang-main-subnet_id" {
+//   value       = aws_nat_gateway.main[*].subnet_id
+//   description = "AWS VPC: VPC main subnet IDs"
+// }
 
+output "avn-as-public-subnet_id" {
+  value       = azurerm_subnet.public[*].subnet_id
+  description = "Azure VNet: Azure public Subnet IDs"
+}
+
+output "avn-as-private-subnet_id" {
+  value       = azurerm_subnet.private[*].subnet_id
+  description = "Azure VNet: Azure private Subnet IDs"
+}
